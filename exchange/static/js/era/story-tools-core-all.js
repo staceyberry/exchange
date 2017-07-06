a947f97025c80c3fd1c7c32b06911ba829e5f2b9
Clarence Davis Jr <cldavisjr@gmail.com>
2017-07-06 17:23:01 -0500

    use osm as the default

--- exchange/static/js/era/story-tools-core-all.js
+++ exchange/static/js/era/story-tools-core-all.js
@@ -24822,9 +24822,9 @@ exports.easingFunctions = {
       defaultMap: function(storymap) {
         storymap.getMap().setView(new ol.View({center: [0, 0], zoom: 3, minZoom: 3, maxZoom: 16}));
         this.setBaseLayer(storymap, {
-          title: 'World Topo Map',
-          type: 'ESRI',
-          name: 'world-topo-map'
+          title: 'OpenStreetMap',
+          type: 'OSM',
+          name: 'osm'
         });
       },
       setBaseLayer: function(storymap, data) {
