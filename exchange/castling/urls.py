1bfba3018164dd61855fd6e891beb693dc9cc88c
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-10-25 20:20:58 -0500

    ensured index.html also pulls from the template

--- exchange/castling/urls.py
+++ exchange/castling/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    # TODO: make these one regex.
     url(r'^castling/$', views.index, name='index'),
+    url(r'^castling/index.html$', views.index, name='index'),
 ] + static('castling', document_root='/mnt/castling/build')
