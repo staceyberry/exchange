ab695cd48ad1747c814a7f31e7702c23c78a7dc6
Clarence Davis Jr <cldavisjr@gmail.com>
2018-01-02 13:55:10 -0600

    Use title rather than name.

--- exchange/tasks.py
+++ exchange/tasks.py
@@ -89,7 +89,7 @@ def create_record(self, id):
         for record in service.servicelayer_set.all():
             item = Record({
                 'uuid': record.uuid,
-                'title': record.name.encode('ascii', 'xmlcharrefreplace'),
+                'title': record.title.encode('ascii', 'xmlcharrefreplace'),
                 'creator': service.owner.username,
                 'record_type': get_types(service.type),
                 'modified': datetime.datetime.now(),
@@ -117,7 +117,7 @@ def create_record(self, id):
     else:
         item = Record({
                 'uuid': service.uuid,
-                'title': service.name.encode('ascii', 'xmlcharrefreplace'),
+                'title': service.title.encode('ascii', 'xmlcharrefreplace'),
                 'creator': service.owner.username,
                 'record_type': get_types(service.type),
                 'modified': datetime.datetime.now(),
